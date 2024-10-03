b761cdf7e33c1ba86143634b3fdf3a2f8e9c7126
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-10-03 15:18:05 -0400

    Script compileWorlds utility

--- scripts/compileWorlds.ts
+++ scripts/compileWorlds.ts
@@ -1,1 +1,24 @@
-console.log(Bun.file("hi"));
+import { readdir } from "node:fs/promises";
+import { json } from "stream/consumers";
+
+const worldFiles = await readdir("./worlds");
+
+let worlds = {};
+
+for (let index = 0; index < worldFiles.length; index++) {
+  const fullName = worldFiles[index];
+  const splitName = fullName.split(".");
+  const placeId = splitName[0];
+  const extension = splitName[1];
+
+  if (extension == "json") {
+    const worldFile = await Bun.file(`./worlds/${fullName}`);
+    const world = await worldFile.json();
+
+    if (typeof world == "object") {
+      worlds[placeId] = world;
+    }
+  }
+}
+
+Bun.write("./worlds.json", JSON.stringify(worlds));
